a77dc15a7b9695f397ef0482ef0564555c404c5c
Lotfullah <andishmandlotfullah21@gmail.com>
2024-10-12 22:30:31 +0530

    Resolved merge conflict in urls.py

--- expense_tracker/urls.py
+++ expense_tracker/urls.py
@@ -1,10 +1,12 @@
 from django.conf import settings
 from django.conf.urls.static import static
+from django.contrib import admin
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.urls import path, include
 
 urlpatterns = [
     path("", include("expenses.urls")),
+    path("admin-view/", admin.site.urls)
 
 ]
 
